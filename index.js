bfca6f1b60c3232f07acdc87844e269f29377ba4
lanasgoddard <lanasgoddard@gmail.com>
2018-12-18 12:12:35 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,26 @@
+const recipes = {cake:'eggs'}
+
+function updateObjectWithKeyAndValue(object, key , value){
+  return Object.assign({}, object, {[key]: value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object
+}
+
+var objects = {cake: 'egg'}
+var newObj = Object.assign({}, object)
+
+function deleteFromObjectByKey(object, key){
+  delete newObj.cake
+  return newObj
+}
+
+const objectt = {cake: 'egg'}
+
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object.key
+  return object
+}
